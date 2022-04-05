d0efa3b36fb3ede14bf9e683f71de9b0e97ef535
Raksix <ermagfurkan7@gmail.com>
2022-04-05 20:50:32 +0300

    new user update :star:

--- services/db/new/user.js
+++ services/db/new/user.js
@@ -1,9 +1,18 @@
+const { userIsThere } = require("../../checkers/user")
 
 
 
 
 module.exports = {
    newUser: async ({ body }) => new Promise((resolve, reject) => {
-      
+      const { name, email, password } = req.body
+      userIsThere({ name, email }).then(user => {
+         if(user){
+            resolve({
+               error: true,
+               message: "Bu kullanıcı zaten var!"
+            })
+         }
+      })
    })
 }
